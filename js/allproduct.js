6d65c2630092a869c8351f26a89c775206a6c91a
gopiyadav989 <gopiyadu17@.com>
2024-09-19 20:25:15 +0530

    sorting & responsiveness

--- js/allproduct.js
+++ js/allproduct.js
@@ -1,28 +1,22 @@
-// Fetch product data from the JSON file
 let products = [];
 
-// Fetch product data from the JSON file and store it in memory
 fetch('../data/product.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
         products = data;
         displayProducts(products);
+        populateFilters();  //to get all options of clothType
     })
     .catch(error => console.error('Error loading the product data:', error));
 
 
 function displayProducts(products) {
     const container = document.getElementById('product-container');
+    container.innerHTML = '';
     products.forEach(product => {
         const productBox = document.createElement('div');
         productBox.className = 'product-box';
 
-        // Wrap the product box in a link
         productBox.innerHTML = `
             <a href="product.html?productId=${product.productId}">
                 <img src="${product.images[0]}" alt="${product.name}">
@@ -61,6 +55,41 @@ function addToCart(event, productId) {
     localStorage.setItem('cart',JSON.stringify(cart));
     alert('Product added to cart!');
     
+}
+
+function populateFilters(){
+    const clothTypeFilter = document.getElementById('clothTypeFilter');
+    const clothType = new Set(products.map(p => p.clothType));
+
+    clothType.forEach(type => {
+        clothTypeFilter.appendChild(new Option(type, type));
+    })
+    // 'option' has 2 parameter
+    // option.value = type;
+    // option.textContent = type;
+
+}
+
+document.getElementById('clothTypeFilter').addEventListener('change',filterAndSortProducts);
+document.getElementById('priceSort').addEventListener('change',filterAndSortProducts);
+
+function filterAndSortProducts(){
+
+    const clothTypeFilter = document.getElementById('clothTypeFilter').value;
+    const priceSort = document.getElementById('priceSort').value;
+
+    // value of All Type is ""(empty string) ie !clothTypeFilter
+    let filteredProducts = products.filter(p => !clothTypeFilter || p.clothType === clothTypeFilter);
+
+    if(priceSort === "asc"){
+        filteredProducts.sort((a,b) => a.price - b.price);
+    }
+    else if(priceSort === 'desc'){
+        filteredProducts.sort((a,b) => b.price - a.price);
+    }
+
+    displayProducts(filteredProducts);
+
 }
 
 
